5f4935abf23c60e293b778998c073f8c261b84bc
Gian vespa <givespas@gmail.com>
2023-08-23 18:06:17 -0500

    feat<app/models/user>: new models

---
+++ app/Models/User.ts
@@ -0,0 +1,26 @@
+import { DateTime } from "luxon";
+import Encryption from '@ioc:Adonis/Core/Encryption'
+import { BaseModel, column } from "@ioc:Adonis/Lucid/Orm";
+
+export default class User extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number;
+
+  @column()
+  public email: string;
+
+  @column()
+  public nick_name: string;
+
+  @column({
+    prepare: (value: string) => Encryption.encrypt(value),
+    consume: (value: string) => Encryption.decrypt(value),
+  })
+  public password: string;
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime;
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime;
+}
